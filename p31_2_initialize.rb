6a19a10a66386c00aa6dfc029da558a2d2b0dbd1
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2023-04-29 00:36:32 +0900

    add file initializeの練習

---
+++ p31_2_initialize.rb
@@ -0,0 +1,11 @@
+class User
+    attr_accessor :name, :address, :email
+
+    def initialize(name, address, email)
+        @name = name
+        @address = address
+        @email = email
+    end
+end
+user = User.new('alice', 'wonderland', 'abcdefg')
+puts user.name
